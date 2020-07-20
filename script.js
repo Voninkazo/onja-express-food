b138df8e284a0542ee93b9f8eca243628b84de56
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-07-20 10:03:07 +0300

    I deleted the form insde of the html and used event delegation to listen for the submit button

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ const btns = document.getElementById('button-add');
 const outerModal = document.querySelector('.outer-modal');
 const innerModal = document.querySelector('.inner-modal');
 
+//show form
+
 btns.addEventListener('click', event => {
-  event.preventDefault();
-  outerModal.classList.add('open');
-  innerModal.innerHTML = `
-  <form>
-				<p>Your name :</p>
-				<input class="input-form" type="text" id="name" name="name" placeholder="Enter your name here"
+	event.preventDefault();
+	outerModal.classList.add('open');
+	innerModal.innerHTML = `
+  <form id="form">
+		<p>Your name :</p>
+			<input class="input-form" type="text" id="name" name="name" placeholder="Enter your name here"
 					required />
-				<p>Please select your dish :</p>
-				<select name="dish" class="select-form" required>
+		<p>Please select your dish :</p>
+				<select name="dish" id="dish" class="select-form" required>
 					<option value="romazava">Romazava</option>
 					<option value="koba">Koba</option>
 					<option value="ravitoto">Ravitoto</option>
@@ -23,7 +25,7 @@ btns.addEventListener('click', event => {
 					<option value="mofo-baolina">Mofo baolina</option>
 					<option value="ranonapango">Ranonapango</option>
 				</select>
-				<p>Please select the size of your plate :</p>
+		<p>Please select the size of your plate :</p>
 				<input type="radio" id="small" name="size" value="small" required />
 				<label for="small">Small</label><br />
 				<input type="radio" id="medium" name="size" value="medium" />
@@ -33,36 +35,43 @@ btns.addEventListener('click', event => {
 				<p>How many pieces do you want to order?</p>
 				<input class="input-form" type="number" id="quantity" name="amount" placeholder="Enter a number here"
 					required />
-				<button class="submitOrder" type="submit">Add this order</button>
-			</form>
-  `
+		<button class="submitOrder" id ="btn-submit" type="submit">Add this order</button>
+</form>
+  `;
 });
+// grab form
 
-const myName = document.getElementById('name');
-const submitBtn = document.getElementById('btn-submit');
 
-const myDish = () => {
-  const myHTML = `
-<div class="order" data-dish="romazava" data-size="large" data-amount="2">
-  <span class="title">
-  ${myName.value}
-  </span>
-  <button class="details">Details</button>
-  <button class="served">Delete order</button>
-  </div>
+const myDish = document.querySelector('[name="dish"]');
+const mySize = document.querySelector('[name="size"]');
+const myQuantity = document.querySelector('#quantity');
+
+// show details 
+
+
+// submit form
+window.addEventListener('click', (event) => {
+	event.preventDefault();
+	if (event.target.matches("button.submitOrder")) {
+		const myName = document.getElementById('name');
+		const myhtml = `
+	<div class="order" data-dish="romazava" data-size="large" data-amount="2">
+		<span class="title">
+			${myName.value}
+		</span>
+		<button class="details">Details</button>
+		<button class="served">Delete order</button>
+	</div>
 `;
-  return myHTML;
-}
-
-submitBtn.addEventListener('click', ($event) => {
-  $event.preventDefault();
-  const myFood = myDish();
-  const postList = document.querySelector('#name-list');
-  postList.insertAdjacentHTML('beforeend', myFood);
-  outerModal.classList.remove('open');
+		event.target = myhtml;
+		const orderList = document.querySelector('.order-list');
+		orderList.insertAdjacentHTML('beforeend', myhtml);
+	}
 });
 
-// const name = document.getElementById('name');
-  // const size = document.querySelector('[name="size"]');
-  // const quantity = document.getElementById('quantity');
-  // const dish = document.getElementById('dish');
+// if (event.target.matches("details")) {
+
+// }
+// if (event.target.matches("served")) {
+
+// }
